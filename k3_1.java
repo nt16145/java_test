e6452aeb8e4dc67a64961ffe68309e1bea3f0739
nt16145 <nt16145@tomakomai.kosen-ac.jp>
2019-07-18 16:36:43 +0900

    complete

--- k3_1.java
+++ k3_1.java
@@ -9,28 +9,86 @@ import javax.servlet.annotation.*;
 @WebServlet(name = "nt16145.k3_1", urlPatterns = { "/nt16145.k3_1" })
 
 public class k3_1 extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-		throws  IOException, ServletException {
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws  IOException, ServletException {
 
-		// 出力形式を設定する
-		response.setContentType("text/html; charset=UTF-8");
-		// 出力先 out を定義する
-		PrintWriter out = response.getWriter();
+      // 出力形式を設定する
+      response.setContentType("text/html; charset=UTF-8");
+      // 出力先 out を定義する
+      PrintWriter out = response.getWriter();
 
-		// HTMLドキュメントを出力する
-		out.println("<html><head>");
-		out.println("<title>k3_1</title>");
-		out.println("</head><body>");
-		out.println("<h1>二項演算</h1><hr>");
-		out.println("二つの数値と演算子を指定してください<br>");
+      // HTMLドキュメントを出力する
+      out.println("<html><head>");
+      out.println("<title>k3_1</title>");
+      out.println("</head><body>");
+      out.println("<h1>二項演算</h1><hr>");
+      out.println("二つの数値と演算子を指定してください<br>");
 
-		// この部分を完成させる
+      // この部分を完成させる
+      String n1_value = null;
+      String n2_value = null;
+      String o_value = null;
+      int operate = 0;
+      out.println("<form action=\"/webapps/nt16145.k3_2\" method=\"GET\">");
+      Cookie[] cookies = request.getCookies();
+      if (cookies != null) {
+        for (int i = 0; i < cookies.length; i++) {
+          String str = cookies[i].getName();
+          // クッキー username が存在する場合，その内容を表示する
+          if (str.equals("number1")) {
+            n1_value = cookies[i].getValue();
+            // 取得した文字列をUTF8でURLデコードする
+            n1_value = URLDecoder.decode(n1_value, "utf-8");
+          }
+
+          if(str.equals("number2")) {
+            n2_value = cookies[i].getValue();
+            n2_value = URLDecoder.decode(n2_value, "utf-8");
+          }
+          if(str.equals("operator")) {
+            o_value = cookies[i].getValue();
+            o_value = URLDecoder.decode(o_value, "utf-8");
+            operate = Integer.parseInt(o_value);
+          }
+        }
+      }
+      out.println("<input type=\"text\" value=" + n1_value +" name=\"number1\" size=10>");
+      out.println("<select name=\"operator\">");
+      if(cookies != null && operate == 1) {
+        out.println("<option value=\"1\" selected>+");
+      }
+      else {
+        out.println("<option value=\"1\">+");
+      }
+      if(cookies != null && operate == 2) {
+        out.println("<option value=\"2\" selected>-");
+      }
+      else {
+        out.println("<option value=\"2\">-");
+      }
+      if(cookies != null && operate == 3) {
+        out.println("<option value=\"3\" selected>*");
+      }
+      else {
+        out.println("<option value=\"3\">*");
+      }
+      if(cookies != null && operate == 4) {
+        out.println("<option value=\"4\" selected>/");
+      }
+      else {
+        out.println("<option value=\"4\">/");
+      }
+      out.println("</select>");
+      out.println("<input type=\"text\" value=" + n2_value + " name=\"number2\" size=10>");
+      out.println("<br>");
+      out.println("<input type=\"submit\" value=\"送信\">");
+      out.println("<input type=\"reset\" value=\"クリア\">");
+      out.println("</form>");
 
 
 
-
-		out.println("<hr>");
-		out.println("No.出席番号　氏名<br>");
-		out.println("</body></html>");
+      out.println("<hr>");
+      out.println("No.23　中村太一<br>");
+      out.println("</body></html>");
     }
 }
--- k3_2.java
+++ k3_2.java
@@ -7,31 +7,85 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "nt16145.k3_2", urlPatterns = { "/nt16145.k3_2" })
-
 public class k3_2 extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-		throws  IOException, ServletException {
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws  IOException, ServletException {
 
-		// 出力形式を設定する
-		response.setContentType("text/html; charset=UTF-8");
-		// 出力先 out を定義する
-		PrintWriter out = response.getWriter();
-		// HTTPリクエストのエンコーディングを UTF-8 とする
-		request.setCharacterEncoding("utf-8");
+      // 出力形式を設定する
+      response.setContentType("text/html; charset=UTF-8");
+      // 出力先 out を定義する
+      PrintWriter out = response.getWriter();
+      // HTTPリクエストのエンコーディングを UTF-8 とする
+      request.setCharacterEncoding("utf-8");
 
-		// HTMLドキュメントを出力する
-		out.println("<html><head>");
-		out.println("<title>k3_2</title>");
-		out.println("</head><body>");
-		out.println("<h1>二項演算</h1><hr>");
+      // HTMLドキュメントを出力する
+      out.println("<html><head>");
+      out.println("<title>k3_2</title>");
+      out.println("</head><body>");
+      out.println("<h1>二項演算</h1><hr>");
 
-		// この部分を完成させる
+      // この部分を完成させる
+      float number1 = 0.0f;
+      float number2 = 0.0f;
+      if(!request.getParameter("number1").equals("")) {
+        try {
+          number1 = Float.parseFloat(request.getParameter("number1"));
+        } catch (NumberFormatException e) {
+          number1 = 1;
+        }
+      }
+      else {
+        number1 = 1;
+      }
+      int operate = Integer.parseInt(request.getParameter("operator"));
+
+      if(!request.getParameter("number2").equals("")) {
+        try {
+          number2 = Float.parseFloat(request.getParameter("number2"));
+        } catch (NumberFormatException e) {
+          number2 = 1;
+        }
+      }
+      else {
+        number2 = 1;
+      }
+      //String enumber1 = URLEncoder.encode(number1, "utf-8");
+      Cookie cookie = new Cookie("number1", String.valueOf(number1));
+      response.addCookie(cookie);
+      //String eoperate = URLEncoder.encode(operate, "utf-8");
+      cookie = new Cookie("operator", String.valueOf(operate));
+      response.addCookie(cookie);
+      //String enumber2 = URLEncoder.encode(number2,"utf-8");
+      cookie = new Cookie("number2",String.valueOf(number2));
+      response.addCookie(cookie);
 
 
+      out.println("<b>(" + number1 + ")");
+      float result = 0;
+      if(operate == 1) {
+        out.println("+");
+        result = number1 + number2;
+      }
+      else if(operate == 2) {
+        out.println("-");
+        result = number1 - number2;
+      }
+      else if(operate == 3) {
+        out.println("*");
+        result = number1 * number2;
+      }
+      else if(operate == 4) {
+        out.println("/");
+        result = number1 / number2;
+      }
+      out.println("(" + number2 + ") = " + result + "</b>");
+      out.println("<br>");
+      out.println("<form action=\"/webapps/nt16145.k3_1\" method=\"GET\">");
+      out.println("<input type=\"submit\" value=\"もどる\">");
+      out.println("</form>");
+      out.println("<hr>");
+      out.println("No.23　中村太一<br>");
+      out.println("</body></html>");
 
-
-		out.println("<hr>");
-		out.println("No.出席番号　氏名<br>");
-		out.println("</body></html>");
     }
 }
